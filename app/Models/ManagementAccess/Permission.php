c691f8f8b4c09eb29f4e888f82265410f2d6ab86
Boedoetmom <trimom806@gmail.com>
2022-09-22 19:29:33 +0700

    model relation has many & has one

---
+++ app/Models/ManagementAccess/Permission.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models\ManagementAccess;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Permission extends Model
+{
+     // use HasFactory;
+   use SoftDeletes;
+
+   public $table = 'permissions';
+
+   protected $date = [
+
+       'create_at',
+       'update_at',
+       'delete_at',
+   ];
+
+   protected $fillable = [
+       'title',
+       'created_at',
+       'updated_at',
+       'deleted_at',
+   ];
+
+   function permission_role() {
+
+    return $this->hasMany('App\Models\ManagementAccess\PermissionRole', 'permission_id');
+}
+
+
+}
---
+++ app/Models/MasterData/Consultation.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models\MasterData;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Consultation extends Model
+{
+   //use HasFactory;
+   use SoftDeletes;
+
+   public $table = 'consultation';
+   
+   protected $date = [
+   
+       'create_at',
+       'update_at',
+       'delete_at',
+   ];
+   
+   protected $fillable = [
+       'name',
+       'created_at',
+       'updated_at',
+       'deleted_at',
+   ];
+
+   function appointment() {
+
+    return $this->hasMany('App\Models\Operational\Appointment', 'consultation_id');
+}
+   
+}
